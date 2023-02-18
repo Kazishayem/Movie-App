03a42288b449cf47d635eb3ffcdb93d1ac327d7f
Kazishayem <kazishayem1234@gmail.com>
2023-02-18 23:31:02 +0600

    first

---
+++ src/stores/counter.js
@@ -0,0 +1,17 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useMovieStore = defineStore('movie', {
+  state: () => ({
+
+  }),
+  getters: {
+
+  },
+  actions:{
+
+  
+
+  }
+})
